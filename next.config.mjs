b46aa800b140c521beb141632123c8661b0eca40
isaccanedo <isaccanedo@gmail.com>
2024-11-11 18:14:52 -0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,149 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  images: {
+    minimumCacheTTL: 2592000, // 30 days
+    remotePatterns: prepareRemotePatterns(),
+  },
+  transpilePackages: ["@trigger.dev/react"],
+  skipTrailingSlashRedirect: true,
+  assetPrefix:
+    process.env.NODE_ENV === "production" &&
+    process.env.VERCEL_ENV === "production"
+      ? process.env.NEXT_PUBLIC_BASE_URL
+      : undefined,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/documents",
+        permanent: false,
+      },
+      {
+        source: "/view/cm2xiaxzo000d147xszm9q72o",
+        destination: "/view/cm34cqqqx000212oekj9upn8o",
+        permanent: false,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer-when-downgrade",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+      {
+        source: "/view/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+      {
+        source: "/services/:path*",
+        has: [
+          {
+            type: "host",
+            value: process.env.NEXT_PUBLIC_WEBHOOK_BASE_HOST,
+          },
+        ],
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+      {
+        source: "/api/webhooks/services/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+    ];
+  },
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/mupdf/*": ["./node_modules/mupdf/dist/*.wasm"],
+    },
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
+function prepareRemotePatterns() {
+  let patterns = [
+    // static images and videos
+    { protocol: "https", hostname: "assets.papermark.io" },
+    { protocol: "https", hostname: "cdn.papermarkassets.com" },
+    { protocol: "https", hostname: "d2kgph70pw5d9n.cloudfront.net" },
+    // twitter img
+    { protocol: "https", hostname: "pbs.twimg.com" },
+    // linkedin img
+    { protocol: "https", hostname: "media.licdn.com" },
+    // google img
+    { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    // papermark img
+    { protocol: "https", hostname: "www.papermark.io" },
+    { protocol: "https", hostname: "app.papermark.io" },
+    // useragent img
+    { protocol: "https", hostname: "faisalman.github.io" },
+    // special document pages
+    { protocol: "https", hostname: "d36r2enbzam0iu.cloudfront.net" },
+  ];
+
+  if (process.env.NEXT_PRIVATE_UPLOAD_DISTRIBUTION_HOST) {
+    patterns.push({
+      protocol: "https",
+      hostname: process.env.NEXT_PRIVATE_UPLOAD_DISTRIBUTION_HOST,
+    });
+  }
+
+  if (process.env.NEXT_PRIVATE_ADVANCED_UPLOAD_DISTRIBUTION_HOST) {
+    patterns.push({
+      protocol: "https",
+      hostname: process.env.NEXT_PRIVATE_ADVANCED_UPLOAD_DISTRIBUTION_HOST,
+    });
+  }
+
+  if (process.env.VERCEL_ENV === "production") {
+    patterns.push({
+      // production vercel blob
+      protocol: "https",
+      hostname: "yoywvlh29jppecbh.public.blob.vercel-storage.com",
+    });
+  }
+
+  if (
+    process.env.VERCEL_ENV === "preview" ||
+    process.env.NODE_ENV === "development"
+  ) {
+    patterns.push({
+      // staging vercel blob
+      protocol: "https",
+      hostname: "36so9a8uzykxknsu.public.blob.vercel-storage.com",
+    });
+  }
+
+  return patterns;
+}
+
+export default nextConfig;
